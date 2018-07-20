ed222c9132007419b6d2cf97100f6e7cd5bd4e83
amir <goudarziha@gmail.com>
2018-07-19 20:50:28 -0400

    2nd commit

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App\Controllers;
+    use Slim\Views\Twig as View;
+    use App\Models\User;
+
+    class HomeController extends Controller {
+
+        public function index($request, $response) {
+            return $this->container->view->render($response, 'home.twig');
+        }
+    }
+?>
